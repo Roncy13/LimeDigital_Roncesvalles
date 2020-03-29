a526023c6d4840693629675bf9d32239033c6308
James Roncesvalles <roncy.pro13@gmail.com>
2020-03-29 19:49:05 +0800

    added post_media tables

---
+++ backend/database/migrations/2020_03_29_114505_create_post_media.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostMedia extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_media', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            $table->string('name');
+            $table->string('path');
+            $table->foreign('post_id')->references('id')->on('post');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_media');
+    }
+}
