5652f494b1c5b1f0f6c4f8a831896281abba6067
James Roncesvalles <roncy.pro13@gmail.com>
2020-03-29 11:39:18 +0800

    allow all origin

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -16,28 +16,8 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $possibleOrigins = [
-            'http://165.22.101.69',
-            'https://165.22.101.69',
-            'http://180.191.184.38'
-        ];
-        $origin = $_SERVER['HTTP_ORIGIN'];
-
-        if (env('APP_ENV') != 'production') {
-            $possibleOrigins[] = 'http://127.0.0.1:4200';
-            $possibleOrigins[] = 'http://localhost:4200';
-        }
-       
-        /*if (in_array($origin, $possibleOrigins)) {
-            return $next($request)
-                ->header('Access-Control-Allow-Origin', $origin)
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        }
-
-        return $next($request);*/
+        
         return $next($request)
-            ->header('Access-Control-Allow-Origin', $origin)
             ->header('Access-Control-Allow-Origin', "*")
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
